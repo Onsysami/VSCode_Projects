77feaac5654fe60fa703f40a75f522ef037b57c6
Onsysami <onsysami.oy@gmail.com>
2023-03-15 21:03:10 -0400

    Traversy

---
+++ Traversy Media - JS Homework/main.js
@@ -0,0 +1,18 @@
+// // Single element selector
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('.container')); 
+
+// // Multiple element selector
+// console.log(document.querySelectorAll('.item'));
+
+// // Loop through
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+// Grab a ul with the class 'elements'
+const ul = document.querySelector('.itmes');
+
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+ul 
