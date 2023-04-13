a179bcd9921fbe9f2b02ec631459422260044c1b
Onsysami <onsysami.oy@gmail.com>
2023-04-13 17:16:14 -0400

    Class-14 done

--- 100devs/class14-materials/temperature-converter/js/main.js
+++ 100devs/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
 //Write your pseduo code first! 
+// Grab temperature
+document.querySelector('#convert').addEventListener('click', convert)
+
+function convert() {
+    let tempFahrenheit = document.querySelector('#tempCelsius').value * 9/5 + 32
+  
+    document.querySelector('#convertedTemperature').innerText = `${tempFahrenheit}°F`
+  }
+// Convert temperature
+// Display new temperature
