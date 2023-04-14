6e01d422de8e5f170860620f1783a38709f2bd89
Onsysami <onsysami.oy@gmail.com>
2023-04-14 11:20:29 -0400

    Finished Class-14

--- 100devs/class14-materials/simple-functions/js/main.js
+++ 100devs/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,17 @@
 //create a function that adds two numbers and alerts the sum
-
+function addSomething(x, y){
+    let sum = x + y
+    alert(sum)
+}
+addSomething(4, 6)
 //create a function that multiplys three numbers and console logs the product
-
+function multiplyThings(a, b, c){
+    let product = a * b * c
+    console.log(product)
+}
+multiplyThings(2, 3, 4)
 //create a function that divides two numbers and returns the ???
+function divideThings(m, n){
+    return m / n
+}
+console.log(divideThings(81, 9))
